7930bf3c55622a57be8d9be3089b71b014126a1f
Lee Iverson <leei@sociologi.ca>
2013-08-22 22:19:31 -0700

    Refer to proper db-6 in postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,6 @@
 
 var exec  = require('child_process').exec;
 
-exec("cp ./deps/db*/build_unix/db.h " + process.installPrefix + '/include/node',
+exec("cp ./deps/db-6*/build_unix/db.h " + process.installPrefix + '/include/node',
      function(err, stdout, stderr) {});
 
